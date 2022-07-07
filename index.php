5dc9ce898386f01924263a7aba2f931c43894408
asfigit <demouserasfi@gmail.com>
2022-07-07 18:44:14 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body bgcolor="black" text="white">
+    <h1 align="center">Test Repo</h1>
+    <hr>
+</body>
+</html>
